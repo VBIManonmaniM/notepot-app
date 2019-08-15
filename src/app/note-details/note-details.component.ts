cfe0f254ad9fc863169e94ad44275aca19747004
Mano M <manonmanim@visualbi.com>
2019-08-15 23:41:35 +0530

    changes

--- src/app/note-details/note-details.component.ts
+++ src/app/note-details/note-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState,INotes } from 'store/store';
 
 @Component({
   selector: 'app-note-details',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-details.component.css']
 })
 export class NoteDetailsComponent implements OnInit {
-
-  constructor() { }
+  @select() notes;
+  @select() activeNoteId;
+  private model: INotes = {
+    id: '',
+    title: '',
+    content: '',
+    lastUpdated: null
+  }
+  constructor(private ngRedux: NgRedux<IAppState>) { }
 
   ngOnInit() {
+    this.model = this.notes.filter(note => note.id === this.activeNoteId);
   }
 
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { ADD_NOTE, DELETE_NOTE, SET_ACTIVE_NOTE } from "./action";
+import { ADD_NOTE, DELETE_NOTE, SET_ACTIVE_NOTE, UPDATE_NOTE } from "./action";
 export interface INotes {
     id: string;
     title: string;
@@ -22,12 +22,29 @@ export const rootReducer = (prevState: IAppState, action): IAppState => {
             action.note.id = Math.random();
             return {
                 ...prevState,
-                notes: prevState.notes.concat(action.note)
+                notes: prevState.notes.concat({
+                    ...action.note,
+                    lastUpdated: new Date()
+                })
             }
         case DELETE_NOTE:
             return {
                 ...prevState,
-                notes: prevState.notes.filter(note => note.id !== action.id)
+                notes: prevState.notes.filter(note => note.id !== action.note.id)
+            }
+        case UPDATE_NOTE:
+            let note = prevState.notes.find(note => note.id === action.note.id);
+            let index = prevState.notes.indexOf(note);
+            return {
+                ...prevState,
+                notes: [
+                    ...prevState.notes.slice(0, index),
+                    {
+                        ...action.note,
+                        lastUpdated: new Date()                    
+                    },
+                    ...prevState.notes.slice(index + 1)
+                ]
             }
         case SET_ACTIVE_NOTE:
             return {
