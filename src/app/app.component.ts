28854456957fff00b8e8af87b4ffb1fa337e8b2f
Mano M <manonmanim@visualbi.com>
2019-08-17 21:35:54 +0530

    Ui fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent extends OnInit {
   showNote = true;
+  @ViewChild('leftArrow') leftArrow: ElementRef;
+  @ViewChild('rightArrow') rightArrow: ElementRef;
+  
+  ngOnInit() {
+    // this.showNoteList();
+  }
 
   hideNoteList() {
     (<any>document.getElementsByTagName('app-note-list')[0]).style.display = 'none';
-    this.showNote = !this.showNote;
+    this.leftArrow.nativeElement.style.display = 'none';
+    this.rightArrow.nativeElement.style.display = 'block';
   }
 
   showNoteList() {
     (<any>document.getElementsByTagName('app-note-list')[0]).style.display = 'block';
-    this.showNote = !this.showNote;
+    this.leftArrow.nativeElement.style.display = 'block';
+    this.rightArrow.nativeElement.style.display = 'none';
   }
 
 }
